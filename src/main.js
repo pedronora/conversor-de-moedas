bba7dda69701ba6e531b80e2d90b701f31602353
Pedro Nora <pedro@nora.vc>
2025-01-29 11:47:09 -0300

    Revertida a inclusão do Vercel SpeedInsigths

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { inject } from "@vercel/analytics";
-import { track } from "@vercel/speed-insights/vue";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
@@ -10,6 +9,5 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 const app = createApp(App);
 
 inject();
-track();
 
 app.mount("#app");
