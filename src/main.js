b3ef97b14313c3794dd301cac4052c2d471c26f0
Pedro Nora <pedro@nora.vc>
2025-01-29 10:32:06 -0300

    Adiciona o Vercel Analytics

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { inject } from '@vercel/analytics'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+inject()
+
+app.mount('#app')
