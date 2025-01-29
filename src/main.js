1a5be92a00d4b939162cb765af16e1d10e8f5402
Pedro Nora <pedro@nora.vc>
2025-01-29 11:39:58 -0300

    Adicionado o Vercel SpeedInsights

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import { inject } from "@vercel/analytics";
+import { track } from "@vercel/speed-insights/vue";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
@@ -9,5 +10,6 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 const app = createApp(App);
 
 inject();
+track();
 
 app.mount("#app");
